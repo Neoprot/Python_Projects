5814e2390ad1a254e5245af8a444e0b11cd9a215
Neoprot <kaua.seichi.gomes@gmail.com>
2024-04-17 22:02:09 -0300

    Learning pymongo

--- integration_with_sqlalchemy/integrationWithSQL/pyMongoApplication.py
+++ integration_with_sqlalchemy/integrationWithSQL/pyMongoApplication.py
@@ -1,1 +1,42 @@
-import pymongo
+import pprint
+
+import pymongo as pyM
+
+client = pyM.MongoClient("mongodb+srv://kauaseichi:jBOaferNhNLcglJa@cluster0.8ld7j9w.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.test
+collection = db.test_collection
+
+post = {
+    'author': 'Mike',
+    'text': 'First mongodb application with python',
+    'tags': ["mongodb", "python3", "pymongo"],
+}
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+print(post_id)
+
+print(db.posts.find_one())
+
+pprint.pprint(db.posts.find_one())
+
+#bulk insert
+new_posts = [{
+    'author': 'Mike',
+    'text': 'another post',
+    'tags': ["bulk", "post", "insert"],},
+    {
+    'author': 'Joao',
+    'text': 'Second mongodb application with python',
+    'tile': 'Mongo is fun'}]
+
+result = posts.insert_many(new_posts)
+print(result.inserted_ids)
+
+print("\n Recuperando dados do Joao")
+pprint.pprint(db.posts.find_one({'author':'Joao'}))
+
+print("\n Recuperando dados do Mongo")
+for post in posts.find():
+    pprint.pprint(post)
---
+++ integration_with_sqlalchemy/integrationWithSQL/secondFilePyMongo.py
@@ -0,0 +1,38 @@
+import pprint
+import pymongo as pyM
+
+client = pyM.MongoClient("mongodb+srv://kauaseichi:jBOaferNhNLcglJa@cluster0.8ld7j9w.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.test
+posts = db.posts
+
+for post in posts.find():
+    pprint.pprint(post)
+
+print(posts.count_documents({}))
+print(posts.count_documents({'author': 'Mike'}))
+
+print(posts.find_one({'tags': 'insert'}))
+
+print("\n Recuperando de Maneira ordenada")
+for post in posts.find({}).sort("author"):
+    pprint.pprint(post)
+
+print("\n Espaço")
+results = db.profiles.create_index([("author", pyM.ASCENDING)], unique=True)
+print(sorted(list(db.profiles.index_information())))
+
+user_profile_user = [
+    {'user_id': 211, 'name': 'Luke'},
+    {'user_id': 212, 'name': 'Jonas'}]
+
+result = db.profiles_user.insert_many(user_profile_user)
+
+collections = db.list_collection_names()
+for collection in collections:
+    pprint.pprint(collection)
+
+for post in posts.find():
+    pprint.pprint(post)
+
+client.drop_database('test')
